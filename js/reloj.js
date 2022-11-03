5ee46b8847ba097c50868c99002f5b3e131178c0
abelcg <abelcg86@gmail.com>
2022-11-03 13:49:44 -0300

    practicaBOM

---
+++ js/reloj.js
@@ -0,0 +1,10 @@
+let time = new Date();
+
+console.log(time);
+console.log(time.getMonth()); // 0-11 corresponde a los meses del año
+console.log(time.getDay()); // 0-6 corresponde a los días de la semana
+console.log(time.getDate()); // 1-31 corresponde a la fecha de cada día
+console.log(time.getFullYear()); //año
+console.log(time.getHours()); //hora
+console.log(time.getMinutes()); //minutos
+console.log(time.getSeconds()); //segundos
